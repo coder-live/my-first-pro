2812617752b368614dc264ae35992761aa403fa3
zjh <zjh169qwer@163.com>
2020-01-13 20:45:26 +0800

    已设置完底栏

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import('views/Home');
+const Buycar = () => import('views/Buycar');
+const Category = () => import('views/Category');
+const Profile = () => import('views/Profile')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    redirect: "/home"
+  },
+  {
+    path: "/home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/buycar',
+    name: 'buycar',
+    component: Buycar
+  },
+  {
+    path: "/category",
+    component: Category
+  },
+  {
+    path: "/profile",
+    component: Profile
   }
 ]
 
---
+++ src/views/Buycar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="buycar">
+    <h1>This is an buycar page</h1>
+  </div>
+</template>
